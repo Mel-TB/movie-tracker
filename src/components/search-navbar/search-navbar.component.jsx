df37c4ba67167049e33513c5d45d0cb0ecb96b56
Mel-TB <melinda.trami@live.fr>
2023-07-07 16:43:38 +0200

    Use useRef for managing a DOM reference - Created a new ref using useRef() inside SearchNavBar component - Added ref prop to the target element - Utilized ref.current to interact with the DOM element

--- src/components/search-navbar/search-navbar.component.jsx
+++ src/components/search-navbar/search-navbar.component.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useRef } from "react";
+
 const SearchNavBar = ({ query, setQuery }) => {
+  const inputElement = useRef(null);
+  // search bar focused and add keydown event
+  useEffect(() => {
+    const callbackListener = (event) => {
+      if (document.activeElement === inputElement.current) return;
+
+      if (event.code === "Enter") {
+        inputElement.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callbackListener);
+
+    return () => document.removeEventListener("keydown", callbackListener);
+  }, [setQuery]);
+
   return (
     <input
       className='search'
@@ -6,6 +25,7 @@ const SearchNavBar = ({ query, setQuery }) => {
       placeholder='Search movies...'
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 };
